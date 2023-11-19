03bd8cf24702941601a1f333eea82b0d9d26455c
Thon <manalangfordy@gmail.com>
2023-11-19 20:29:13 +0800

    Changes to server and Navigation bar

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const { log } = require("console");
 const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public"), { setHeaders: (res, path) => res.type('text/css') }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"))
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const server = app.listen(5000);
-const portNum = server.address().port;
-console.log(`port is open on 127.0.0.1:${portNum}`);
+const server = app.listen(5000, () => {
+    const portNum = server.address().port;
+    console.log(`Server is running on http://127.0.0.1:${portNum}`);
+});
